df70351e486c381b5e89943c9f16d7d5efb728a8
Máté Szabó <mateszabo141@gmail.com>
2020-07-12 16:13:23 +0200

    wrote function to modify attributes with negative value

--- modifier.py
+++ modifier.py
@@ -1,12 +1,15 @@
 import re
 
 def general_pattern(input_string):
-    return bool(re.search(r'\=\+?\d\.\d', input_string))
+    return bool(re.search(r'\=\+?\-?\d\.\d', input_string))
 
 def pattern_with_plus_sign(input_string):
     return bool(re.search(r'\+\d\.\d', input_string))
 
-def pattern_without_plus_sign(input_string):
+def pattern_with_minus_sign(input_string):
+    return bool(re.search(r'\-\d\.\d', input_string))
+
+def pattern_without_sign(input_string):
     return bool(re.search(r'\d\.\d', input_string))
 
 def modify_file(filename):
@@ -28,7 +31,9 @@ def modify_file(filename):
                         for sub_word in sub_words:
                             if pattern_with_plus_sign(sub_word):
                                 content[iterator] = content[iterator].replace(sub_word, '+9.9')
-                            elif pattern_without_plus_sign(sub_word):
+                            elif pattern_with_minus_sign(sub_word):
+                                content[iterator] = content[iterator].replace(sub_word, '+9.9')
+                            elif pattern_without_sign(sub_word):
                                 content[iterator] = content[iterator].replace(sub_word, '9.9')
             iterator += 1
 
@@ -57,3 +62,5 @@ if not modify_file("xplayersL2.upl"):
     print("Failed to open xplayersL2.upl\nMake sure you execute the program in the \"Unreal Tournament 2004\System\" directory")
 if not modify_file("xaplayersl3.upl"):
     print("Failed to open xaplayersl3.upl\nMake sure you execute the program in the \"Unreal Tournament 2004\System\" directory")
+
+input("Press Enter to close the application")
